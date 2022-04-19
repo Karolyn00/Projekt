520b21372b62e505bc601796728db5416af6b59d
SBsebi2 <102352497+SBsebi2@users.noreply.github.com>
2022-04-19 17:36:58 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -18,5 +18,12 @@ function handleSubmit(e) {
 		})
 }
 document.querySelector("form").addEventListener("submit", handleSubmit); // Nastavime formular, aby pri submit udalosti spustil nasu handleSubmit funkciu
-
+function demo(e){
+	e.preventDefault();
+	document.querySelector("textarea").value ="VES v1.6 500 500 \nCLEAR #FF0000 \nFILL_TRIANGLE 200 100 400 300 300 300 #0000FF \nFILL_CIRCLE 200 100 50 #00FF00 \nFILL_RECT 400 100 150 200 #00FF00 \nCIRCLE 300 200 100 1 #FFFFFF \nTRIANGLE 50 100 200 300 150 200 1 #00FF00 \nRECT 200 100 300 100 1 #000000 \nLINE 0 0 599 499 1 #000000 ";
+	document.getElementById("output").src="https://c.tenor.com/nbB1q7b_TvcAAAAd/abstract.gif";
+} 
+	
+document.querySelector("form").addEventListener("submit", handleSubmit); 
+document.querySelector("#demo").addEventListener("click", demo)
 
